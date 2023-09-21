e933218158f1f2944f9a14632de3cc923e450065
Alepha Null <HashTag>
2023-09-22 00:08:36 +0530

    Added blurred background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+	<section class="banner-container first">
+		<h1>Client Onboardings made Easy</h1>
+		<h2>Powered by AI</h2>
+	</section>
+</body>
+
+<style>
+	:root {
+		font-family: 'Raleway', sans-serif;
+	}
+
+	section {
+		height: 90vh;
+		width: 90vw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.banner-container {
+		background-image: url('images/lights.svg');
+		background-repeat: no-repeat;
+		background-size: 100vh;
+	}
+	body {
+		background: black;
+		color: white;
+	}
+
+	h1,
+	h2 {
+		position: relative;
+		top: 400px;
+	}
+
+	@media (max-width: 610px) {
+		h1,
+		h2 {
+			top: 350px;
+			font-size: 1.4em;
+		}
+	}
+	@media (max-width: 434px) {
+		h1,
+		h2 {
+			top: 300px;
+			font-size: 1em;
+		}
+	}
+</style>
